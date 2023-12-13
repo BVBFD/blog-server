687c0aa6e848672368a6903ac88b3aeb0245459a
lsevina126 <lsevina126@gmail.com>
2023-12-13 09:18:03 +0900

    While(connect): Add while sentence to connect mongodb for faster intial connect to mongodb

--- app.js
+++ app.js
@@ -16,13 +16,39 @@ const ContactDatasModel = require("./models/contactDatasModel.js");
 
 dotenv.config();
 
-mongoose
-  .connect(process.env.MONGO_DB_URL, {
-    dbName: "myFirstDatabase",
-    keepAlive: true,
-    keepAliveInitialDelay: 30000,
-  })
-  .then(() => console.log("Mongo DB Start!"));
+async function dbConnect() {
+  let maxRetries = 10;
+  let currentRetry = 0;
+
+  while (currentRetry < maxRetries) {
+    try {
+      await mongoose.connect(`${process.env.MONGO_DB_URL}`, {
+        dbName: "myFirstDatabase",
+        connectTimeoutMS: 1000,
+        serverSelectionTimeoutMS: 1000,
+        maxPoolSize: 10,
+        minPoolSize: 1,
+        keepAlive: true,
+        keepAliveInitialDelay: 30000,
+      });
+
+      console.log("MongoDB connected successfully!!");
+      break;
+    } catch (error) {
+      currentRetry++;
+      console.log("MongoDB connect failed!!");
+      // Add some delay before retrying
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+    }
+  }
+
+  if (currentRetry === maxRetries) {
+    // Handle the case where max retries are reached
+    console.error("Max retries reached. MongoDB connection failed.");
+  }
+}
+
+dbConnect();
 
 const app = express();
 app.use(express.json());
